dd30a6257af89609a347e9ad40daaac67bbee0f7
AnishMandal939 <mandalktaanees12@gmail.com>
2024-03-20 23:03:56 +0545

    Add cloudinary package and configure cloudinary*** ***Update user route and controller*** ***Update user model to include token signing methods*** ***Update app.ts to include course and order routers - main order with notification

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,30 @@
 import express from "express";
-import { registrationUser } from "../controllers/user.controller";
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updateAvatar, updatePassword, updateUserInfo } from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 // router
 const userRouter = express.Router();
 
-userRouter.route("/registration").post(registrationUser);
+userRouter.post("/registration", registrationUser);
+// activation  request | POST
+userRouter.post("/activate-user", activateUser);
+
+// login request | POST || logout request | GET || refresh token | GET
+userRouter.post("/login",loginUser);
+userRouter.get("/logout", isAuthenticated , authorizeRoles("admin") ,logoutUser);
+userRouter.get("/refresh", updateAccessToken);
+
+// user profile
+userRouter.get("/me", isAuthenticated, getUserInfo);
+
+userRouter.post("/social-auth", socialAuth);
+
+userRouter.put("/update-user-info",isAuthenticated, updateUserInfo);
+userRouter.put("/update-user-password",isAuthenticated, updatePassword);
+userRouter.put("/update-user-avatar",isAuthenticated, updateAvatar);
+
+
+
+
 
 export default userRouter;
---
+++ server/utils/jwt.ts
@@ -0,0 +1,64 @@
+require("dotenv").config(); // load .env file
+import { Response } from "express";
+import { IUser } from "../models/user.model";
+import Jwt, { Secret } from "jsonwebtoken";
+import { redis } from "./redis";
+
+// interface
+interface ITokenOptions{
+    expiresIn: Date;
+    maxAge: number;
+    httpOnly: boolean;
+    sameSite: 'lax' | 'strict' | 'none' | undefined;
+    secure?: boolean;
+}
+
+ // parse env variable to integrate with fallback values
+  const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || "300", 10);
+  const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || "1200", 10);
+
+ // options for cookies
+ export const accessTokenOptions: ITokenOptions = {
+     expiresIn: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+     maxAge: accessTokenExpire * 60 * 60 * 1000,
+     httpOnly: true,
+     sameSite: "lax",
+     // secure: process.env.NODE_ENV === "production" ? true : false,
+ };
+
+ export const refreshTokenOptions: ITokenOptions = {
+     expiresIn: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+     maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+     httpOnly: true,
+     sameSite: "lax",
+     // secure: process.env.NODE_ENV === "production" ? true : false,
+ };
+
+// create access token
+export const sendToken = (user: IUser, statusCode: number, res: Response) => {
+    const accessToken = user.SignAccessToken();
+    const refreshToken = user.SignRefreshToken();
+
+    // upload session to redis
+    // redis.set(refreshToken, user._id.toString(), "EX", 60 * 60 * 24 * 30); // 30 days
+    redis.set(user._id, JSON.stringify(user) as any); // 30 days
+
+    // only set secure to true in production
+    if (process.env.NODE_ENV === "production") {
+        accessTokenOptions.secure = true;
+        // refreshTokenOptions.secure = true;
+    }
+
+    // set cookies
+    res.cookie("access_token", accessToken, accessTokenOptions);
+    res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+    // send response
+    res.status(statusCode).json({
+        success: true,
+        accessToken,
+        user,
+    });
+
+   
+};
